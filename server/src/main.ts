a359d7b69f14fdf552d01e403865a82e4aabd962
2donny <2donny@naver.com>
2021-07-09 19:19:33 +0000

    middleware : morgan

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,9 +1,11 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import { AppModule } from './app/app.module';
+import morgan from 'morgan';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.use(morgan('dev'));
   app.useGlobalPipes(new ValidationPipe());
   await app.listen(80);
 }

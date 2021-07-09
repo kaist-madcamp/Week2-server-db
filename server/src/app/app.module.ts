407952b25208f5eb9d86ff5814384e206daee34d
2donny <2donny@naver.com>
2021-07-09 18:44:13 +0000

    app module

---
+++ server/src/app/app.module.ts
@@ -0,0 +1,39 @@
+import {
+  Module,
+  NestModule,
+  MiddlewareConsumer,
+  RequestMethod,
+} from '@nestjs/common';
+import { JwtModule } from '../jwt/jwt.module';
+import { JwtMiddleware } from '../jwt/jwt.middleware';
+import { ConfigModule } from '@nestjs/config';
+import { UsersModule } from '../users/users.module';
+import { AppController } from './app.controller';
+import * as Joi from 'joi';
+
+@Module({
+  imports: [
+    ConfigModule.forRoot({
+      validationSchema: Joi.object({
+        JWT_KEY: Joi.string().required(),
+      }),
+      isGlobal: true,
+      cache: true,
+      envFilePath: '.env',
+    }),
+    JwtModule.forRoot({
+      jwt_key: process.env.JWT_KEY,
+    }),
+    UsersModule,
+  ],
+  providers: [],
+  controllers: [AppController],
+})
+export class AppModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer.apply(JwtMiddleware).forRoutes({
+      path: '*',
+      method: RequestMethod.ALL,
+    });
+  }
+}

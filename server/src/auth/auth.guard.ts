cf60793ce1de551c84aa3730d1e521f592ce65dd
2donny <2donny@naver.com>
2021-07-10 12:04:52 +0000

    auth decorator

---
+++ server/src/auth/auth.guard.ts
@@ -0,0 +1,12 @@
+import { CanActivate, ExecutionContext, Injectable } from '@nestjs/common';
+
+@Injectable()
+export class AuthGuard implements CanActivate {
+  canActivate(context: ExecutionContext): boolean {
+    const req = context.switchToHttp().getRequest();
+    if (req.headers.user) {
+      return true;
+    }
+    return false;
+  }
+}

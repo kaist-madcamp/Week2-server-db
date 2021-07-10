3bc75cfdd4b3ce6dfc82adf7394e2e71b1fee773
2donny <2donny@naver.com>
2021-07-10 12:10:55 +0000

    post module

--- server/src/posts/posts.controller.ts
+++ server/src/posts/posts.controller.ts
@@ -1,15 +1,72 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Post,
+  Put,
+  UseGuards,
+} from '@nestjs/common';
+import { User } from '@prisma/client';
+import { AuthUser } from 'src/auth/auth-user.decorator';
+import { AuthGuard } from 'src/auth/auth.guard';
 import { CreatePostInput, CreatePostOutput } from './dtos/create-post.dto';
+import { DeletePostInput, DeletePostOutput } from './dtos/delete-post.dto';
+import { EditPostInput, EditPostOutput } from './dtos/edit-post.dto';
+import {
+  FindPostByIdInput,
+  FindPostByIdOutput,
+} from './dtos/find-post-by-id.dto';
+import {
+  FindPostsByCategoryInput,
+  FindPostsByCategoryOutput,
+} from './dtos/find-posts-by-category.dto';
 import { PostsService } from './posts.service';
 
 @Controller('posts')
 export class PostsController {
   constructor(private readonly postsService: PostsService) {}
 
-  @Post('create')
+  @Get('/:id')
+  async findPostById(
+    @Param() params: FindPostByIdInput,
+  ): Promise<FindPostByIdOutput> {
+    return this.postsService.findPostById(params);
+  }
+
+  @Get('/all/:categoryId')
+  async findPostsByCategory(
+    @Param() params: FindPostsByCategoryInput,
+  ): Promise<FindPostsByCategoryOutput> {
+    return this.postsService.findPostsByCategory(params);
+  }
+
+  @Post('')
+  @UseGuards(AuthGuard)
   async createPost(
+    @AuthUser() authUser: User,
     @Body() createPostInput: CreatePostInput,
   ): Promise<CreatePostOutput> {
-    return this.postsService.createPost(createPostInput);
+    return this.postsService.createPost(authUser, createPostInput);
+  }
+
+  @Delete(':id')
+  @UseGuards(AuthGuard)
+  async deletePost(
+    @AuthUser() owner: User,
+    @Param() params: DeletePostInput,
+  ): Promise<DeletePostOutput> {
+    return this.postsService.deletePost(owner, params);
+  }
+
+  @Put(':id')
+  @UseGuards(AuthGuard)
+  async editPost(
+    @AuthUser() owner: User,
+    @Param('id') id: string,
+    @Body() editPost: EditPostInput,
+  ): Promise<EditPostOutput> {
+    return this.postsService.editPost(owner, id, editPost);
   }
 }
--- server/src/posts/posts.service.ts
+++ server/src/posts/posts.service.ts
@@ -1,21 +1,104 @@
 import { Injectable } from '@nestjs/common';
+import { User } from '@prisma/client';
 import { PrismaService } from 'src/prisma/prisma.service';
 import { CreatePostInput, CreatePostOutput } from './dtos/create-post.dto';
+import { DeletePostInput, DeletePostOutput } from './dtos/delete-post.dto';
+import { EditPostInput, EditPostOutput } from './dtos/edit-post.dto';
+import {
+  FindPostByIdInput,
+  FindPostByIdOutput,
+} from './dtos/find-post-by-id.dto';
+import {
+  FindPostsByCategoryInput,
+  FindPostsByCategoryOutput,
+} from './dtos/find-posts-by-category.dto';
 
 @Injectable()
 export class PostsService {
   constructor(private readonly prismaService: PrismaService) {}
 
+  public async findPostById(
+    params: FindPostByIdInput,
+  ): Promise<FindPostByIdOutput> {
+    try {
+      const post = await this.prismaService.post.findUnique({
+        where: {
+          id: +params.id,
+        },
+      });
+      console.log(post);
+      if (!post) {
+        return {
+          ok: false,
+          error: '존재하지 않는 게시글입니다.',
+        };
+      }
+      return {
+        ok: true,
+        post,
+      };
+    } catch (error) {
+      console.log(error);
+      return {
+        ok: false,
+        error,
+      };
+    }
+  }
+
+  public async findPostsByCategory(
+    params: FindPostsByCategoryInput,
+  ): Promise<FindPostsByCategoryOutput> {
+    try {
+      const posts = await this.prismaService.post.findMany({
+        where: {
+          categoryId: +params.categoryId,
+        },
+        select: {
+          id: true,
+          title: true,
+          subtitle: true,
+          authorId: true,
+        }
+      });
+
+      console.log(posts);
+      return {
+        ok: true,
+        posts,
+      };
+    } catch (error) {
+      return {
+        ok: false,
+        error,
+      };
+    }
+  }
+
   public async createPost(
+    authUser: User,
     createPostInput: CreatePostInput,
   ): Promise<CreatePostOutput> {
     try {
-      const post = await this.prismaService.post.create({
-        data: {
-          ...createPostInput,
+      const category = await this.prismaService.category.findUnique({
+        where: {
+          name: createPostInput.categoryName,
         },
       });
-
+      if (!category) {
+        return {
+          ok: false,
+          error: '존재하지 않는 카테고리입니다. 카테고리를 생성해주세요!',
+        };
+      }
+      const post = await this.prismaService.post.create({
+        data: {
+          title: createPostInput.title,
+          subtitle: createPostInput.subtitle,
+          categoryId: category.id,
+          authorId: authUser.id,
+        },
+      });
       console.log(post);
 
       return {
@@ -23,6 +106,80 @@ export class PostsService {
       };
     } catch (error) {
       console.log(error);
+      return {
+        ok: false,
+        error,
+      };
+    }
+  }
+
+  public async deletePost(
+    owner: User,
+    { id }: DeletePostInput,
+  ): Promise<DeletePostOutput> {
+    try {
+      const post = await this.prismaService.post.findUnique({
+        where: {
+          id: +id,
+        },
+      });
+      if (!post) {
+        return {
+          ok: false,
+          error: '존재하지 않는 게시글입니다.',
+        };
+      }
+      if (post.authorId !== owner.id) {
+        return {
+          ok: false,
+          error: '권한이 없습니다.',
+        };
+      }
+      await this.prismaService.post.delete({
+        where: {
+          id: +id,
+        },
+      });
+      return {
+        ok: true,
+      };
+    } catch (error) {
+      return {
+        ok: false,
+        error,
+      };
+    }
+  }
+
+  public async editPost(
+    owner: User,
+    id: string,
+    editPostInput: EditPostInput,
+  ): Promise<EditPostOutput> {
+    try {
+      const post = await this.prismaService.post.findUnique({
+        where: {
+          id: +id,
+        },
+      });
+      if (!post) {
+        return {
+          ok: false,
+          error: '존재하지 않는 게시물입니다.',
+        };
+      }
+
+      if (post.authorId !== owner.id) {
+        return {
+          ok: false,
+          error: '권한이 없습니다.',
+        };
+      }
+
+      return {
+        ok: true,
+      };
+    } catch (error) {
       return {
         ok: false,
         error,

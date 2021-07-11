8bbbf08b3fe7178be996a56127e07afc384082ab
2donny <2donny@naver.com>
2021-07-11 11:19:37 +0000

    make fields optional

--- server/src/posts/dtos/edit-post.dto.ts
+++ server/src/posts/dtos/edit-post.dto.ts
@@ -1,14 +1,17 @@
 import { CoreOutput } from './../../common/dtos/output.dto';
-import { IsString } from 'class-validator';
+import { IsOptional, IsString } from 'class-validator';
 
 export class EditPostInput {
   @IsString()
+  @IsOptional()
   title?: string;
 
   @IsString()
+  @IsOptional()
   subtitle?: string;
 
   @IsString()
+  @IsOptional()
   categoryName?: string;
 }
 
